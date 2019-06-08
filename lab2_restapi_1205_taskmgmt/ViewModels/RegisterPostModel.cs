b1d438567dc9efbe6daa2fb9db237f484982e972
Moldovan <moldovan_cristian_radu@yahoo.ro>
2019-06-08 11:42:34 +0300

    the teacher made me do it

--- lab2_restapi_1205_taskmgmt/ViewModels/RegisterPostModel.cs
+++ lab2_restapi_1205_taskmgmt/ViewModels/RegisterPostModel.cs
@@ -14,6 +14,6 @@ namespace lab2_restapi_1205_taskmgmt.ViewModels
         [EmailAddress]
         public string Email { get; set; }
         [StringLength(150, MinimumLength = 6)]
-        public string Password { get; set; }        
+        public string Password { get; set; }
     }
 }
